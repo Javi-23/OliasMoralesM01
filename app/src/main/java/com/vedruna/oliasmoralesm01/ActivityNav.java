392cc8a9997e922825eec359ee41690dbaf25470
Javi-23 <javieroliasmorales@gmail.com>
2024-01-14 18:40:24 +0100

    Ej 3 - V.2

--- app/src/main/java/com/vedruna/oliasmoralesm01/ActivityNav.java
+++ app/src/main/java/com/vedruna/oliasmoralesm01/ActivityNav.java
@@ -14,14 +14,22 @@ public class ActivityNav extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nav);
 
+        // Obtiene una referencia al componente BottomNavigationView definido en activity_nav.xml
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
+
+        // Establece el elemento seleccionado por defecto en la barra de navegación inferior
         bottomNavigationView.setSelectedItemId(R.id.navigation_home);
 
+        // Obtiene el fragmento de host de navegación del contenedor definido en activity_nav.xml
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_Root);
+
+        // Asegura que el fragmento de host de navegación no sea nulo
         assert navHostFragment != null;
+
+        // Obtiene el controlador de navegación asociado al fragmento de host de navegación
         NavController navController = navHostFragment.getNavController();
 
-        // Obtén el nombre de usuario de la actividad anterior
+
         Bundle bundle = getIntent().getExtras();
         String text = bundle.getString("nombreUsuario");
         Bundle args = new Bundle();
@@ -30,14 +38,19 @@ public class ActivityNav extends AppCompatActivity {
         ContadorFragment contadorFragment = new ContadorFragment();
         contadorFragment.setArguments(args);
 
+        // Configura el listener para manejar los eventos de selección de elementos en la barra de navegación inferior
         bottomNavigationView.setOnItemSelectedListener(item -> {
+            // Verifica el ID del elemento seleccionado y navega a la pantalla correspondiente utilizando el NavController
             if (item.getItemId() == R.id.navigation_home) {
                 navController.navigate(R.id.homeFragment);
             } else if (item.getItemId() == R.id.navigation_contador) {
                 navController.navigate(R.id.contadorFragment);
             } else {
+                // En este caso, navega a un fragmento llamado exitFragment (puedes ajustar este nombre según tu configuración)
                 navController.navigate(R.id.exitFragment);
             }
+
+            // Devuelve true para indicar que el evento de selección ha sido manejado correctamente
             return true;
         });
     }
